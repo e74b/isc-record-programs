2dc782663fb7cdda909138e67ca87fc8de7b9f0f
Joel Singh <185637278+e74b@users.noreply.github.com>
2025-07-02 04:40:18 +0530

    Updated Dec2Oct.java to match algorithm.

--- 12-term1/Dec2Oct.java
+++ 12-term1/Dec2Oct.java
@@ -1,18 +1,23 @@
 import java.util.*;
 
 public class Dec2Oct {
+	static String oct = "";
 
+	public static void main (String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter number to convert: ");
+		int n = input.nextInt();
+		decitooct(n);
 
-	static String convert (int number) {
-		if (number < 8) // Base case
-			return "" + number;
-		return "" + convert(number / 8) + (number % 8);
+		System.out.println("Octal: 0o" + oct);
 	}
 
-	public static void main(String args[]) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter number:");
-		int n = input.nextInt();
-		System.out.println("Octal form: " + convert(n));
+	static void decitooct (int number) {
+		if (number < 8) {
+			oct = number + oct; // Base case
+		} else {
+			oct = number % 8 + oct;
+			decitooct(number / 8); // Recursive case
+		}
 	}
 }
